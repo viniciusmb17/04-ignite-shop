6d297931123f5d39643d11415b11df3dfa2a9c69
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-02-14 14:07:57 -0300

    feat: estrutura da home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg.src} alt="" width={129.74} height={52} />
+        <Image src={logoImg} alt="" width={129.74} height={52} />
       </Header>
       <Component {...pageProps} />
     </Container>
