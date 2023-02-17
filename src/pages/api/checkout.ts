83766680cc8704546ecfc21887b92cc4ae7f5f18
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-02-17 12:00:34 -0300

    feat: stripe checkout api

---
+++ src/pages/api/checkout.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { stripe } from '../../lib/stripe'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const priceId = 'price_1MbSKuCFRGecX3HZzIf5lg9o'
+
+  const succesUrl = `${process.env.NEXT_URL}/success`
+  const cancelUrl = `${process.env.NEXT_URL}/`
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    success_url: succesUrl,
+    cancel_url: cancelUrl,
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+  })
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url,
+  })
+}
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -16,10 +16,14 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
+  }
   const { isFallback } = useRouter()
 
   if (isFallback) {
@@ -37,7 +41,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button onClick={handleBuyProduct}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -72,6 +76,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
           currency: 'BRL',
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, // 1 hour
