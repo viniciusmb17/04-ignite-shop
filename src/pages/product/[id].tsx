cefdd7c4ba1f12419bc2ac24d651c30fc4b964ce
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-02-16 23:49:32 -0300

    feat: estrutura da pagina de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,29 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 
-export default function Teste() {
+export default function Product() {
   const { query } = useRouter()
-  return <div>Product: {JSON.stringify(query)}</div>
+
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+          repudiandae fugit nemo neque eos qui nihil quaerat asperiores,
+          eligendi accusamus reprehenderit tenetur assumenda praesentium dolores
+          laudantium odio est itaque libero.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
