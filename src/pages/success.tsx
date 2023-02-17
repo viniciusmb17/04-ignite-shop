a11f7bb05532d65990a359ae59456cb2683c6640
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-02-17 20:32:35 -0300

    feat: page success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link'
+import { ImageContainer, SuccessContainer } from '../styles/pages/sucess'
+
 export default function Success() {
-  return <div>Success</div>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Diego Fernandes</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
