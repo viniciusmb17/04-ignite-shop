03d49b3c9dce86db9c53c9cd19e7c58a90d13277
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-02-22 21:34:59 -0300

    style: success page image shadow

--- src/styles/pages/sucess.ts
+++ src/styles/pages/sucess.ts
@@ -2,11 +2,13 @@ import { styled } from '..'
 
 export const SuccessContainer = styled('main', {
   display: 'flex',
+  width: '100%',
+  maxWidth: 'calc(100vw - ((100vw - 1180px) /2))',
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'flex-start',
   margin: '0 auto',
-  paddingTop: '4.5rem',
+  paddingTop: '42px',
   height: 656,
 
   h1: {
@@ -40,22 +42,26 @@ export const SuccessContainer = styled('main', {
 
 export const ImagesContainer = styled('div', {
   display: 'flex',
+  width: '100%',
+  height: '100vh',
   flexDirection: 'row',
   justifyContent: 'center',
   alignItems: 'center',
   padding: 0,
+
+  overflowX: 'auto',
 })
 
 export const ImageContainer = styled('div', {
   width: '100%',
-  minWidth: 140,
-  maxWidth: 140,
-  height: 140,
+  minWidth: 145,
+  maxWidth: 145,
+  height: 145,
   background: 'linear-gradient(180deg, #1ea483, #7465d4)',
   borderRadius: 99,
   padding: '0.25rem',
   margin: '0 -1.625rem',
-  boxShadow: '0px 0px 60px rgba(0, 0, 0, 0.8)',
+  boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.8)',
 
   display: 'flex',
   alignItems: 'center',
